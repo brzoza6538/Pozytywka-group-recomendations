aade1720224fd47ca6644cce6dad3844fc5689a0
jhapunik <01169173@pw.edu.pl>
2025-01-06 14:27:38 +0100

    added a starting point for the idea of dynamic recommendations

--- model/module2_prediction.py
+++ model/module2_prediction.py
@@ -5,6 +5,10 @@ class PredictionModel:
     def __init__(self):
         self.weights = None
 
+        # do rozszerzenia do edytowania w czasie rzeczywistym
+        self.playlist = []
+        self.history = []
+
     def prepare_data_for_user(self, user_id, user_session, tracks):
         '''
         Przygotowuje dane treningowe dla użytkownika
@@ -102,13 +106,12 @@ class PredictionModel:
         X = np.hstack([np.ones((X.shape[0], 1)), X])  # Dodaj stałą do cech
         self.weights = np.linalg.pinv(X.T @ X) @ X.T @ y
 
-    def predict(self, song_id, tracks, artists):
+    def predict(self, song_id, tracks):
         '''
          Przewiduje ocenę dla piosenki na podstawie song_id.
 
         :param song_id: ID piosenki
         :param tracks: Dane utworów (track_id -> features)
-        :param artists: Dane artystów (artist_id -> genres)
         :return: Wynik predykcji
         '''
         if self.weights is None:
@@ -139,3 +142,57 @@ class PredictionModel:
 
         prediction = features @ self.weights
         return 1 / (1 + np.exp(-prediction))  # Skalowanie do przedziału [0, 1]
+    
+
+    # rozszerzenie do edytowania w czasie rzeczywistym
+
+    def replay_track(self, track_id):
+        if track_id in self.playlist:
+            self.history.append((track_id, 'replay'))
+            self.playlist.append(track_id)
+
+    def skip_track(self, track_id):
+        if track_id in self.playlist:
+            self.history.append((track_id, 'skip'))
+            self.playlist.remove(track_id)
+    
+#    sw
+# to bardzo podstawowy sposob generowania rekomendacji, trzeba efekt skip replay jakos dodawac do training?
+#     def generate_recommendations(self, history, tracks):
+#         '''
+#         Generuje rekomendacje na podstawie historii przesłuchanych utworów.
+        
+#         :param history: Lista krotek (track_id, akcja)
+#         :param tracks: Dane utworów (track_id -> features)
+#         :return: Lista zrekomendowanych track_id
+#         '''
+#         # Analiza historii
+#         popular_tracks = self.analyze_popularity(history, tracks)
+#         recommended_tracks = []
+        
+#         # Generowanie rekomendacji
+#         for track_id in popular_tracks:
+#             if track_id not in [h[0] for h in history]:
+#                 recommended_tracks.append(track_id)
+        
+#         return recommended_tracks
+
+
+# def analyze_popularity(self, history, tracks):
+#         '''
+#         Analizuje historię użytkownika i wybiera najbardziej popularne utwory.
+        
+#         :param history: Lista krotek (track_id, akcja)
+#         :param tracks: Dane utworów (track_id -> features)
+#         :return: Posortowana lista popularnych track_id
+#         '''
+#         # Liczenie, które utwory były najczęściej odtwarzane ponownie
+#         replay_count = {}
+#         for track_id, action in history:
+#             if action == 'replay':
+#                 replay_count[track_id] = replay_count.get(track_id, 0) + 1
+        
+#         # Sortowanie według popularności
+#         sorted_tracks = sorted(replay_count.items(), key=lambda item: item[1], reverse=True)
+#         return [track_id for track_id, _ in sorted_tracks]
+
