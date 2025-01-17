311995d9cf0ea1fed38299f2eddda04f1bd6067a
brzoza6538 <m3.brzezinski@gmail.com>
2025-01-17 15:54:02 +0100

    fixing accuracy test

--- model/init_gen.py
+++ model/init_gen.py
@@ -10,11 +10,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 import numpy as np
+import itertools
 
 from collections import Counter
 
 import random
 
+#TODO - add session related weights calibration
+#TODO - check what track attr to use
 
 def get_tracks_by_ids(track_ids):
     tracks = []
@@ -26,13 +29,14 @@ def get_tracks_without_mentioned_by_ids(track_ids):
     tracks = Track.query.filter(~Track.track_id.in_(track_ids)).all()
     return [track.to_dict() for track in tracks]
 
-def get_type_of_tracks(user_id, time_border, event_type):
+def get_type_of_tracks(user_id, event_type, from_time, to_time=datetime.utcnow()):
    sessions = (
       Session.query
       .filter(
          (Session.user_id == user_id) & 
          (Session.event_type == event_type) &
-         (Session.timestamp < time_border)
+         (Session.timestamp > from_time) &
+         (Session.timestamp <= to_time)
       )
       .all()
    )
@@ -51,7 +55,9 @@ def get_type_of_tracks(user_id, time_border, event_type):
 
 class GroupReccomendations:
     def __init__(self, user_ids):
-        self._time_border = datetime.utcnow() - timedelta(days=90)
+        self._time_window_start   = datetime.utcnow() - timedelta(days=180)
+        self._time_window_end   = datetime.utcnow()
+
         self._users_favourite_tracks_amount = 10000 #100
         self._cluster_recommendation = 100 #50
         self._taste_groups = 30 #3 #TODO zależne od ilości osób? 
@@ -64,7 +70,6 @@ class GroupReccomendations:
 
         self.user_ids = user_ids
 
-        self._test_tracks = []
         self.recommendations = self.create_recommendations()
 
     def get(self):
@@ -128,9 +133,9 @@ class GroupReccomendations:
         '''
         score songs of a user with given user_id
         '''
-        started_tracks = get_type_of_tracks(user_id, self._time_border, "play")
-        skipped_tracks = get_type_of_tracks(user_id, self._time_border, "skip")
-        liked_tracks = get_type_of_tracks(user_id, self._time_border, "like")
+        started_tracks = get_type_of_tracks(user_id, "play", self._time_window_start, self._time_window_end)
+        skipped_tracks = get_type_of_tracks(user_id, "skip", self._time_window_start, self._time_window_end)
+        liked_tracks = get_type_of_tracks(user_id, "like", self._time_window_start, self._time_window_end)
 
         for track_id in started_tracks.keys():
             record = (
@@ -141,7 +146,7 @@ class GroupReccomendations:
             started_tracks[track_id] = 1 / (1 + np.exp(-record))
         return started_tracks
 
-    def get_top_tracks(self, test_size=0):
+    def get_top_tracks(self):
         '''
         get best regarded songs of users
         '''
@@ -157,13 +162,8 @@ class GroupReccomendations:
                     connected_scores[track_id] += user_scores[track_id]
 
         sorted_items = sorted(connected_scores.items(), key=lambda item: item[1], reverse=True)
-        top_tracks = [track_id for track_id, score in sorted_items[:self._users_favourite_tracks_amount+test_size]]
+        top_tracks = [track_id for track_id, score in sorted_items[:self._users_favourite_tracks_amount]]
 
-        for _ in range(test_size):  # test related
-            if top_tracks:
-                random_track = random.choice(top_tracks)
-                self._test_tracks.append(random_track) 
-                top_tracks.remove(random_track)  
 
         return top_tracks
 
@@ -342,68 +342,43 @@ class GroupReccomendations:
         return recommendations
 
 
-
     def test_create_recommendations(self):
-        '''
-        main method creating tracks for class, connecting all methods 
-        '''
-        #np. user 101 - 1136 piosenek, 303 - 415, 202 - 530 
-        users_favourite_tracks_amount_p = [1000, 100000]
-        cluster_recommendation_p = [100, 1000]
-        taste_groups_p = [30, 50]
 
-        records = []
+        #(_time_window_start, _time_window_end)
 
+        time_start =[360, 180]
+        time_end = [60]
+        users_favourite_tracks_amount_p = [100, 1000, 5000]
+        cluster_recommendation_p = [30, 100]
+        taste_groups_p = [5, 15, 30]
+
+        constraints = list(itertools.product(time_start, time_end, users_favourite_tracks_amount_p, cluster_recommendation_p, taste_groups_p))
+        results = []
         self._final_playlist_length = 100
-        user_test_size = 100
 
+        for setup in constraints:
+            self._users_favourite_tracks_amount = setup[2]
+            self._cluster_recommendation = setup[3]
+            self._taste_groups = setup[4] 
 
-        for users_favourite_tracks_amount in users_favourite_tracks_amount_p:
-            for cluster_recommendation in cluster_recommendation_p:
-                for taste_groups in taste_groups_p:
+            self._time_window_start   = datetime.utcnow() - timedelta(days=setup[0])
+            self._time_window_end   = datetime.utcnow() - timedelta(days=setup[1])
 
-                    self._users_favourite_tracks_amount = users_favourite_tracks_amount
-                    self._cluster_recommendation = cluster_recommendation
-                    self._taste_groups = taste_groups 
+            start = datetime.utcnow()
+            recommendations = self.create_recommendations()
+            end = datetime.utcnow()
 
-                    tracks_ids = self.get_top_tracks(user_test_size)
-                    # print(len(tracks_ids))
+            self._time_window_start   = datetime.utcnow() - timedelta(days=setup[1])
+            self._time_window_end   = datetime.utcnow()
+            self._users_favourite_tracks_amount = 10000
+            test_tracks = self.get_top_tracks()
 
-                    print("\n-------------------------\n")
+            duplicates = 0
+            for track in recommendations:
+                if(track in test_tracks):
+                    duplicates += 1
+            results.append(f"\n{setup}\n   -  {len(test_tracks)}   - {duplicates} : {end - start}\n")
+            print(f"\n{setup}\n   -  {len(test_tracks)}   - {duplicates} : {(end - start).total_seconds()}\n")
 
-                    tracks_data = get_tracks_by_ids(tracks_ids)
+        return results
 
-                    track_clusters = self.cluster_tracks(tracks_data) # group most liked music of users using Kmeans
-
-                    recommendations = []
-
-                    for cluster in track_clusters:
-                        recommendations += self.recommend_tracks_for_cluster(cluster) # recommend tracks for each individual cluster
-                    
-                    data = {}
-                    for user_id in self.user_ids:
-                        predictions = self.evaluate_tracks(self.get_weighed_tracks(user_id), recommendations)
-                        data = {key: data.get(key, 0) + predictions.get(key, 0) for key in data | predictions} # predict score of each recommended track for each user using decision tree
-
-                    recommendations = sorted(data, key=data.get, reverse=True)[:self._final_playlist_length] 
-
-                    duplicates = 0
-                    for track in recommendations:
-                        if(track in self._test_tracks):
-                            duplicates += 1
-                    if (duplicates/len(recommendations) >= 0):
-                        records.append((f"{self._users_favourite_tracks_amount}  - {self._cluster_recommendation} - {self._taste_groups}", {round(duplicates/len(recommendations), 4)}))
-
-                    print(duplicates)
-                    print(len(recommendations))
-                    
-                    print(f"{self._users_favourite_tracks_amount}  - {self._cluster_recommendation} - {self._taste_groups}")
-
-
-        print("\n\n\n------------------\n\n\n\n")
-        sorted_records = sorted(records, key=lambda record: next(iter(record[1])))
-        for record in sorted_records:
-            print(record)
-
-        
-        return recommendations
